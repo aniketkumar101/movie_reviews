cc971e7f165cb0404d3d50147de8d0920cbec744
aniket kumar <aniket.787068@gmail.com>
2025-04-16 12:25:31 +0530

    update streamlit file

--- app.py
+++ app.py
@@ -4,17 +4,13 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 
-# Load model and tokenizer
 model = load_model('my_model.h5')
 
-# Load the tokenizer (make sure you saved it during training)
 with open('tokenizer.pkl', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-# Set the maximum sequence length (same as used during training)
-MAX_LEN = 200  # Change it to your model's max length
+MAX_LEN = 200
 
-# Streamlit UI
 st.title("🎬 Movie Review Sentiment Analyzer")
 st.write("Enter your movie review to predict its sentiment.")
 
@@ -24,17 +20,13 @@ if st.button("Predict"):
     if user_input.strip() == "":
         st.warning("Please enter a valid review.")
     else:
-        # Tokenize and pad the input
         seq = tokenizer.texts_to_sequences([user_input])
         padded = pad_sequences(seq, maxlen=MAX_LEN)
 
-        # Predict sentiment
         print(model.predict(padded),"9999")
-        prediction = model.predict(padded)[0][0]  # Assuming binary classification
+        prediction = model.predict(padded)[0][0]
         print(prediction)
 
-
-        # Interpret the result
         sentiment = "Positive 😊" if prediction > 0.5 else "Negative 😞"
         st.success(f"Sentiment: {sentiment} (Confidence: {prediction:.2f})")
 
